10da9ebf3ce6f46168335ea74189eee85ac99192
Akash Withanaarachchi <e19453@eng.pdn.ac.lk>
2024-06-16 01:47:01 +0530

    added gitignore and fixed bug in connection

--- Helpdesk/db_conn.php
+++ Helpdesk/db_conn.php
@@ -1,11 +1,19 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php'; // Include Composer's autoloader
+
+use Dotenv\Dotenv;
+
+// Load environment variables from .env file
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $sname = "localhost";
 $db_name = "techub";
 
 // Fetch credentials from environment variables
-$uname = getenv('DB_USERNAME');
-$password = getenv('DB_PASSWORD');
+$uname = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
 
 $conn = mysqli_connect($sname, $uname, $password, $db_name);
 
