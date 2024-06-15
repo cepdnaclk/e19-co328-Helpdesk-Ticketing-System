e1ef0b6ffc5b972c0371351cf9460f983510f8b8
Akash Withanaarachchi <e19453@eng.pdn.ac.lk>
2024-06-16 00:53:45 +0530

    Hide sql login username and password in env file

--- Helpdesk/db_conn.php
+++ Helpdesk/db_conn.php
@@ -1,9 +1,11 @@
-<?php 
+<?php
 
 $sname = "localhost";
-$uname = "root";
-$password = "";
 $db_name = "techub";
+
+// Fetch credentials from environment variables
+$uname = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
 
 $conn = mysqli_connect($sname, $uname, $password, $db_name);
 
